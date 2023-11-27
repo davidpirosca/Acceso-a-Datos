99ea8c73e9b300da7091d1a71a8cea82ff18bd02
David Cristian Pirosca <davidpirosca@gmail.com>
2023-11-27 20:50:21 +0100

    conexionDBEx Lista

---
+++ ConexionDBEx/src/main/java/conexionDB/DatosConexion.java
@@ -0,0 +1,24 @@
+package conexionDB;
+
+public class DatosConexion {
+
+    private final String DB_URL = "jdbc:mysql://localhost:3306/jcvd";
+    private final String USER = "user";
+    private final String PASS = "1234";
+
+    public DatosConexion() {
+    }
+
+    public String getDB_URL() {
+        return DB_URL;
+    }
+
+    public String getUSER() {
+        return USER;
+    }
+
+    public String getPASS() {
+        return PASS;
+    }
+
+}
